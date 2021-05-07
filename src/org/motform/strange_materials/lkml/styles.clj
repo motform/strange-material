f5d2b329c8833dbbb82050f3e6498ac22a66efbd
motform <love.lagerkvist@protonmail.com>
2021-05-07 09:33:11 +0200

    Clean up stylesheet

--- src/org/motform/strange_materials/lkml/styles.clj
+++ src/org/motform/strange_materials/lkml/styles.clj
@@ -19,108 +19,101 @@
 (def styles
   (css/register
    ::style
-   (let [padding 10]
-     {::padding padding
+   {".root" {:-fx-background-color (colors :background)
+             :-fx-font-family      "MD System"
+             :-fx-font-size        20}
 
-      "@font-face" {:-fx-font-family "MD System"
-                    :src "url(\"resources/fonts/MDSystemTest-Regular.ttf\")"}
+    ".scroll-bar:vertical"   {:-fx-pref-width  10}
+    ".scroll-bar:horizontal" {:-fx-pref-height 10}
 
-      ".root" {:-fx-background-color (colors :background)
-               :-fx-font-family      "MD System"
-               :-fx-font-size        20}
+    ".track" {:-fx-background-color (colors :background)}
+    ".thumb" {:-fx-background-color (colors :background) :-fx-background-radius 5}
 
-      ".scroll-bar:vertical"   {:-fx-pref-width  10}
-      ".scroll-bar:horizontal" {:-fx-pref-height 10}
+    ".increment-button" {:-fx-background-color (colors :background) :-fx-padding 0   :-fx-border-color (colors :background)}
+    ".decrement-button" {:-fx-background-color (colors :background) :-fx-padding -50 :-fx-border-color (colors :background)}
+    ".decrement-arrow"  {:-fx-padding 0 :-fx-border-width 0 :-fx-border-color (colors :background) :-fx-shape " "}
+    ".increment-arrow"  {:-fx-padding 0 :-fx-border-width 0 :-fx-border-color (colors :background) :-fx-shape " "}
+    ".scroll-arrow"     {:-fx-padding 0}
 
-      ".track" {:-fx-background-color (colors :background)}
-      ".thumb" {:-fx-background-color (colors :background) :-fx-background-radius 5}
+    ".text-field" {:-fx-background-color  (colors :background)
+                   :-fx-text-fill         (colors :foreground)
+                   :-fx-padding           [15 15 15 15]
+                   :-fx-font-family       "Inconsolata"
+                   :-fx-font-size         25
+                   :-fx-background-radius 0}
 
-      ".increment-button" {:-fx-background-color (colors :background) :-fx-padding 0   :-fx-border-color (colors :background)}
-      ".decrement-button" {:-fx-background-color (colors :background) :-fx-padding -50 :-fx-border-color (colors :background)}
-      ".decrement-arrow"  {:-fx-padding 0 :-fx-border-width 0 :-fx-border-color (colors :background) :-fx-shape " "}
-      ".increment-arrow"  {:-fx-padding 0 :-fx-border-width 0 :-fx-border-color (colors :background) :-fx-shape " "}
-      ".scroll-arrow"     {:-fx-padding 0}
+    ".text-area" {:-fx-background-color "red"}
 
-      ".text-field" {:-fx-background-color  (colors :background)
-                     :-fx-text-fill         (colors :foreground)
-                     :-fx-padding           [15 15 15 15]
-                     :-fx-font-family      "Inconsolata"
-                     :-fx-font-size        25
-                     :-fx-background-radius 0}
+    ".command-line" {:-fx-padding    [0 0 0 0]
+                     :-fx-background (colors :frame)}
 
-      ".text-area" {:-fx-background-color "red"}
+    ".command-history" {:-fx-padding 10
+                        :-fx-spacing 10
+                        "-tab" {:-fx-padding          [5 10]
+                                :-fx-border-width     1
+                                :-fx-border-color     (colors :frame)
+                                :-fx-border-radius    12
+                                :-fx-font-size        16
+                                :-fx-font-family      "Inconsolata"
+                                ":hover" {:-fx-background-color (colors :sidebar)
+                                          :-fx-background-radius 12}
+                                "-selected" {:-fx-background-color (colors :sidebar)
+                                             :-fx-background-radius 12}}}
 
-      ".command-line" {:-fx-padding    [0 0 0 0]
-                       :-fx-background (colors :frame)}
+    ".sidebar" {:-fx-border-color (colors :foreground)
+                :-fx-border-width [1 0 0 0]
 
-      ".command-history" {:-fx-padding 10
-                          :-fx-spacing 10
-                          "-tab" {:-fx-padding [5 10]
-                                  :-fx-border-width     1
-                                  :-fx-border-color     (colors :frame)
-                                  :-fx-border-radius    12
-                                  :-fx-font-size        16
-                                  :-fx-font-family      "Inconsolata"
-                                  ":hover" {:-fx-background-color (colors :sidebar)
-                                            :-fx-background-radius 12}
-                                  "-selected" {:-fx-background-color (colors :sidebar)
-                                               :-fx-background-radius 12}}}
+                "-container" {"-label"
+                              {:-fx-padding   [20 5 5 15]
+                               :-fx-font-size 12
+                               "-text" {:-fx-text-fill (colors :foreground)}}}
 
-      ".sidebar" {:-fx-border-color (colors :foreground)
-                  :-fx-border-width [1 0 0 0]
+                "-list" {:-fx-padding 10
+                         :-fx-spacing 5
+                         "-sublist" {:-fx-spacing 5}}}
 
-                  "-container" {"-label"
-                                {:-fx-padding          [20 5 5 15]
-                                 :-fx-font-size        12
-                                 "-text" {:-fx-text-fill (colors :foreground)}}}
+    ".system-call-item" {:-fx-padding            [3 0 3 10]
+                         :-fx-border-width       1
+                         :-fx-border-color       (colors :frame)
+                         :-fx-border-radius      10
+                         :-fx-background-radius  10
+                         :-fx-font-size          14
+                         :-fx-font-family        "Inconsolata"
+                         ":hover"    {:-fx-background-color  (colors :sidebar)}
+                         "-selected" {:-fx-background-color  (colors :sidebar)
+                                      :-fx-padding           [3 0 3 10]
+                                      :-fx-font-size         14
+                                      :-fx-border-color      (colors :frame)
+                                      :-fx-border-width      1
+                                      :-fx-background-radius 10
+                                      :-fx-border-radius     10
+                                      :-fx-font-family       "Inconsolata"}}
 
-                  "-list" {:-fx-padding 10
-                           :-fx-spacing 5
-                           "-sublist" {:-fx-spacing 5}}}
+    ".std-out" {"-container" {:-fx-padding 10}
+                "-item" {:-fx-padding [2 0 2 10]
+                         "-text" {:-fx-font-size   14
+                                  :-fx-font-family "Inconsolata"}}}
 
-      ".system-call-item" {:-fx-padding          [3 0 3 10]
-                           :-fx-border-width     1
-                           :-fx-border-color     (colors :frame)
-                           :-fx-border-radius    10
-                           :-fx-background-radius  10
-                           :-fx-font-size          14
-                           :-fx-font-family      "Inconsolata"
-                           ":hover"    {:-fx-background-color (colors :sidebar)}
-                           "-selected" {:-fx-background-color (colors :sidebar)
-                                        :-fx-padding          [3 0 3 10]
-                                        :-fx-font-size        14
-                                        :-fx-border-color     (colors :frame)
-                                        :-fx-border-width     1
-                                        :-fx-background-radius  10
-                                        :-fx-border-radius    10
-                                        :-fx-font-family      "Inconsolata"}}
-
-      ".std-out" {:-fx-padding          0
-                  "-container" {:-fx-padding 10}
-                  "-item" {:-fx-padding [2 0 2 10]
-                           "-text" {:-fx-font-size   14
-                                    :-fx-font-family "Inconsolata"}}}
-
-      ".email" {:-fx-background-color "transparent"
-                :-fx-font-family "Inconsolata"
-                "-container" {:-fx-border-color     (colors :frame)
-                              :-fx-border-width     [1 0 0 0]}
-                "-list" {:-fx-spacing 5
-                         :-fx-padding [10 0 0 0]}
-                "-view" {:-fx-padding       10
-                         :-fx-border-width  1
-                         :-fx-border-radius 10
-                         :-fx-background-radius 10
-                         :-fx-border-color (colors :frame)
-                         :-fx-font-size    15
-                         ":hover"    {:-fx-background-color (colors :selection)}
-                         "-topic"    {:-fx-padding [0 0 5 0]}
-                         "-selected" {:-fx-background-color (colors :selection)}
-                         "-meta"     {:-fx-text-fill (colors :foreground)
-                                      :-fx-font-size 11}
-                         "-body"     {:-fx-padding          [10 0 0 10]
-                                      :-fx-font-size 15
-                                      :-fx-font-family      "Inconsolata"}
-                         "-offset-button" {:-fx-alignment "CENTER"
-                                           :-fx-text-fill "red"
-                                           :-fx-font-size 10}}}})))
+    ".email" {:-fx-background-color "transparent"
+              :-fx-font-family      "Inconsolata"
+              "-container" {:-fx-border-color (colors :frame)
+                            :-fx-border-width [1 0 0 0]}
+              "-list" {:-fx-spacing 5
+                       :-fx-padding [10 0 0 0]}
+              "-view" {:-fx-padding           10
+                       :-fx-border-width      1
+                       :-fx-border-radius     10
+                       :-fx-background-radius 10
+                       :-fx-border-color      (colors :frame)
+                       :-fx-font-size         15
+                       ":hover"    {:-fx-background-color (colors :selection)}
+                       "-topic"    {:-fx-padding [0 0 5 0]}
+                       "-selected" {:-fx-background-color (colors :selection)}
+                       "-meta"     {:-fx-text-fill (colors :foreground)
+                                    :-fx-font-size 11}
+                       "-body"     {:-fx-padding     [10 0 0 10]
+                                    :-fx-font-size   15
+                                    :-fx-font-family "Inconsolata"}
+                       "-offset-button" {:-fx-alignment "CENTER"
+                                         :-fx-text-fill "red"
+                                         :-fx-font-size 10}}}}))
